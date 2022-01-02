df6a5578e3c3dfc1f1fd985278548b13068ff6c2
Ingo <thewatcher@watchersnet.de>
2022-01-02 16:23:30 +0100

    updated basic tests

--- YAF.UnitTests/YAF.Tests.BasicTests/ContainerTests.cs
+++ YAF.UnitTests/YAF.Tests.BasicTests/ContainerTests.cs
@@ -1,7 +1,7 @@
 /* Yet Another Forum.NET
  * Copyright (C) 2003-2005 Bjørnar Henden
  * Copyright (C) 2006-2013 Jaben Cargman
- * Copyright (C) 2014-2021 Ingo Herbote
+ * Copyright (C) 2014-2022 Ingo Herbote
  * https://www.yetanotherforum.net/
  *
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -63,7 +63,6 @@ namespace YAF.Tests.BasicTests
         /// </summary>
         [Test]
         [Description("The container is available to send digest in background.")]
-        [Ignore("Doesnt work yet")]
         public void Container_Is_Available_To_Send_Digest_In_Background()
         {
             using (new HttpSimulator("/", TestConfig.TestFilesDirectory).SimulateRequest())
